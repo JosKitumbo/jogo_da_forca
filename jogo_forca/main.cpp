8d12ee9e720c0bc77b881b26e533fa96ab992452
JosKitumbo <168676607+JosKitumbo@users.noreply.github.com>
2024-05-06 14:53:29 +0200

    quase

--- jogo_forca/main.cpp
+++ jogo_forca/main.cpp
@@ -42,157 +42,125 @@ void word(){// colocando as palavras e verificando se são semelhantes ou não
  char palavra_player[50];
  char letra;   //jogada
 
-         ifstream MyReadFile("palavras.txt.txt");
-         string mytext;
-         int contagem_txt=0;
+ ifstream MyReadFile("palavras.txt.txt");
+ string mytext;
+ int contagem_txt=0;
 
-        while(MyReadFile>>mytext)
-        {
-            contagem_txt++;
+ while(MyReadFile>>mytext){
+        contagem_txt++;
+    }
+ MyReadFile.close();
+ srand(time(NULL));
+ int x = rand()%contagem_txt;
+ ifstream MyReadFile1("palavras.txt.txt");
 
-        }
-          MyReadFile.close();
-         srand(time(NULL));
-       int x = rand()%contagem_txt;
-       ifstream MyReadFile1("palavras.txt.txt");
-
-        for(int i=1; i<x; i++)
-        {
-            MyReadFile1>>mytext;
-        }
-       MyReadFile1.close();
-       for(int i=0; i<mytext.size(); i++)
-        {
+ for(int i=1; i<x; i++){
+      MyReadFile1>>mytext;
+    }
+ MyReadFile1.close();
+ for(int i=0; i<mytext.size(); i++){
             palavra[i]=mytext[i];
+    }
+ cout<<endl;
+ cout<<"Um animal de: " << strlen(palavra)<< " letras "<<endl;
 
-        }
-        cout<<endl;
-        cout<<"Um animal de: " << strlen(palavra)<< " letras "<<endl;
-
-cout<<endl;
-string barras[mytext.size()];
-barras[0]=palavra[0];
-cout<<barras[0]<<" ";
-for(int i=1; i<mytext.size(); i++)
-        {
+ cout<<endl;
+ string barras[mytext.size()];
+ barras[0]=palavra[0];
+ cout<<barras[0]<<" ";
+ for(int i=1; i<mytext.size(); i++){
            barras[i]="_";
            cout<<barras[i]<<" ";
-        }
-cout<<endl;
+    }
+ cout<<endl;
  cout<<"Entre as letras da palavra : "<<endl;//momento do jogador;
+
  do{
 
 
-    for (int i=1; i<=7; i++)
-        {   contagem=0;
-            cout<<"Tentativas : "<<tentativa<<"/7"<<endl;
-            cout<<endl;
-            char repetir_letra='*';
-            repetir_letra=letra;
-            cin>>letra;
-           if(letra==repetir_letra)
-            {
-                cout<<"Tentou esta letra antes! "<<endl;
-                cout<<"Queira refazer ";
-                break;
+  for (int i=1; i<=7; i++){
+     contagem=0;
+     cout<<"Tentativas : "<<tentativa<<"/7"<<endl;
+     cout<<endl;
+     char repetir_letra='*';
+     repetir_letra=letra;
+     cin>>letra;
+     if(letra==repetir_letra){
+            cout<<"Tentou esta letra antes! "<<endl;
+            cout<<"Queira refazer ";
+            break;
+        }
+     bool iguais_letra=false;
+     for(int j=1; j<strlen(palavra); j++){
+            if(palavra[j]==letra ){
+                contagem++;
+                if(contagem==1){
+                    cout<< "A letra '" << letra << "' se encontra na posicao " << j+1 <<endl;
+                    indice_letras++;
+                    palavra_player[indice_letras]=letra;
+                    barras[j]=letra;
+                    iguais_letra=true;
+                }
+                if(contagem==2){
+                    cout<< "aussi na posicao " <<j+1 <<endl;
+                    indice_letras++;
+                    palavra_player[indice_letras]=letra;
+                    barras[j]=letra;
+                    iguais_letra=true;
+                }
+                if(contagem==3){
+                    cout<< "e" << " na palavra na posicao " << j+1 <<endl;
+                    indice_letras++;
+                    palavra_player[indice_letras]=letra;
+                    barras[j]=letra;
+                    iguais_letra=true;
+                }
+
             }
-            bool iguais_letra=false;
-
-            for(int j=1; j<strlen(palavra); j++)
-            {
-
-                if(palavra[j]==letra )
-                    {   contagem++;
-                        if(contagem==1)
-                        {
-                        cout<< "A letra '" << letra << "' se encontra na posicao " << j+1 <<endl;
-                        indice_letras++;
-                        palavra_player[indice_letras]=letra;
-                        barras[j]=letra;
-                        iguais_letra=true;
-
-                        }
-                        if(contagem==2)
-                        {
-                        cout<< "aussi na posicao " <<j+1 <<endl;
-                        indice_letras++;
-                        palavra_player[indice_letras]=letra;
-                        barras[j]=letra;
-
-
-                        iguais_letra=true;
-                        }
-                        if(contagem==3)
-                        {
-                        cout<< "e" << " na palavra na posicao " << j+1 <<endl;
-                        indice_letras++;
-                        palavra_player[indice_letras]=letra;
-                        barras[j]=letra;
-
-                        iguais_letra=true;
-                        }
-
-
-                    }
-
 
 
             }
-            for(int i=0; i<mytext.size(); i++)
-        {
-
+    for(int i=0; i<mytext.size(); i++){
            cout<<barras[i]<<" ";
         }
-                  cout<<endl;
-            if(iguais_letra==true)
-                {
+            cout<<endl;
+    if(iguais_letra==true){
                     cout<<"Congrats!!" <<endl;
                     break;
-                }
-            else
-                {
-                    cout<<"Mistake!!" <<endl;
-                    tentativa++;
-                }
-            if(tentativa==7)//perder por tentativas esgotadas
-                {
+      }else{
+            cout<<"Mistake!!" <<endl;
+            tentativa++;
+        }
+    if(tentativa==7){//perder por tentativas esgotadas
                   sair_boucle=true;//sair da boucle(quebrar ela)
                   break;
                 }
-            if(indice_letras==strlen(palavra))
-                {
+    if(indice_letras==strlen(palavra)){
                     sair_boucle1=true;
                     break;
-                }
+        }
 
-                }
-                  cout<<endl;
-                 if(sair_boucle1==true)
-                    {
-                        break;
-                    }
-                 if(sair_boucle==true)//perder por tentativas esgotadas
-                    {
-                        break;//sair da boucle(quebrar ela)
-                    }
-
-
+     }
+    cout<<endl;
+    if(sair_boucle1==true){
+            break;
+        }
+    if(sair_boucle==true){//perder por tentativas esgotadas
+            break;//sair da boucle(quebrar ela)
+        }
 
    }while(indice_letras!=strlen(palavra));
 
        cout<<endl;
 
-
-        if(indice_letras==strlen(palavra))
-            {
-                cout<<"Ganhou!! "<<endl;
-                cout<<"A palavra era : ";
-                for(int i=0; i<strlen(palavra); i++){
-                    palavra_player[i]=palavra[i];
-                    cout<<palavra_player[i];
+    if(indice_letras==strlen(palavra)){
+            cout<<"Ganhou!! "<<endl;
+            cout<<"A palavra era : ";
+            for(int i=0; i<strlen(palavra); i++){
+                palavra_player[i]=palavra[i];
+                cout<<palavra_player[i];
                 }
-            }else
-                {
+            }else{
                     cout<<"Tentativas esgotadas "<<". Perdeu!! "<<endl;
                     cout<<"A palavra era: ";
                     for(int i=0; i<strlen(palavra); i++){
@@ -200,7 +168,7 @@ cout<<endl;
                             cout<<palavra_player[i];
                       }
                 }
-}
+  }
 
 };
 
